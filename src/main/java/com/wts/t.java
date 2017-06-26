0ba5f051a4b862d2d20b2f93a284d0c56a21f510
王天硕 <nelsonnick@126.com>
2017-06-26 19:09:43 +0800

    更新

--- src/main/java/com/wts/t.java
+++ src/main/java/com/wts/t.java
@@ -1,28 +1,46 @@
 package com.wts;
 
-import java.io.BufferedInputStream;
-import java.io.FileInputStream;
-import java.io.InputStream;
+import java.io.*;
+import java.net.URL;
 import java.util.Iterator;
 import java.util.Properties;
 
 
 public class t {
-  public static void main(String[] args) {
-    Properties prop = new Properties();
-    try {
-      InputStream is = t.class.getClassLoader().getResourceAsStream("info.properties");
-      InputStream in = new BufferedInputStream(new FileInputStream("resources/info.properties"));
-      prop.load(in);     ///加载属性列表
-      Iterator<String> it = prop.stringPropertyNames().iterator();
-      while (it.hasNext()) {
-        String key = it.next();
-        System.out.println(key + ":" + prop.getProperty(key));
-      }
-      in.close();
 
-    } catch (Exception e) {
-      System.out.println(e);
+    private static void saveProperty() throws Exception {
+        // 保存文件
+        Properties propertie = new Properties();
+        propertie.setProperty("passwd", "111111111");
+        ClassLoader classLoader = t.class.getClassLoader();
+        URL resource = classLoader.getResource("info.properties");
+        String path = resource.getPath();
+
+        FileOutputStream outputFile = new FileOutputStream(path);
+        propertie.store(outputFile, "danciben");
+        outputFile.close();
     }
-  }
+
+
+    public static void getProperty() {
+        Properties prop = new Properties();
+        try {
+            ClassLoader classLoader = t.class.getClassLoader();
+            URL resource = classLoader.getResource("info.properties");
+            String path = resource.getPath();
+            InputStream in = classLoader.getResourceAsStream("info.properties");
+
+            prop.load(in);     ///加载属性列表
+            prop.setProperty("passwd", "11111");
+
+            in.close();
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        saveProperty();
+    }
 }
