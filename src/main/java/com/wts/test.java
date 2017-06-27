8156d3f06895b60429bc0bc57cd1cd6cdffe4f1d
王天硕 <nelsonnick@126.com>
2017-06-27 10:39:14 +0800

    。。

--- src/main/java/com/wts/test.java
+++ src/main/java/com/wts/test.java
@@ -2,18 +2,23 @@ package com.wts;
 
 
 import com.wts.entity.PersonLH;
+import com.wts.util.PropKit;
 import org.apache.http.impl.client.CloseableHttpClient;
 
 import java.util.List;
+import java.util.Properties;
 
 import static com.wts.service.common.login;
 import static com.wts.util.Export.ExportLHResult;
 import static com.wts.util.Import.ImportLH;
+import static com.wts.util.PropKit.getString;
+import static com.wts.util.PropKit.loadProps;
 
 public class test {
   public static void main(String[] args) throws Exception{
-    CloseableHttpClient client= login("hyddj","888888");
-    List<PersonLH> persons =ImportLH("ddj");
+    Properties properties=loadProps(PropKit.class.getClassLoader().getResource("info.properties").getPath());
+    CloseableHttpClient client= login(getString(properties,"userid"),getString(properties,"passwd"));
+    List<PersonLH> persons =ImportLH(getString(properties,"result"));
     ExportLHResult(client,persons,"201706");
   }
 }
